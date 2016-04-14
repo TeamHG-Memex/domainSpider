e667539108ad5e7b0112169b8b9d057ea56efff0
Jason Hopper <jason.t.hopper@gmail.com>
2016-04-14 10:22:56 -0300

    added some comments, and print lines. This version also works better. Added better arabic extraction

--- submitTranslation.py
+++ submitTranslation.py
@@ -22,8 +22,8 @@ A quick and dirty script that will:
 con = db.connect("data.db")
 cur = con.cursor()
 
-cur.execute("UPDATE INTO urlData SET englishText=''")
-con.commit()
+#cur.execute("UPDATE urlData SET englishText=''")
+#con.commit()
 
 cur.execute("SELECT url,plainText FROM urlData where englishText=''")# and domain not like 'http://www.khilafah.com%'")
 results = cur.fetchall()
@@ -32,13 +32,35 @@ results = cur.fetchall()
 batchTranslateDelim = "\n<--------------------------------------------->\n"
 specialChars = """ !@#$%^&*()_+-=[]\{}|;':",./<>? """
 
+totalResults = len(results)
+
 for i,result in enumerate(results[:]):
-    print("\n\n\n\n\n")
+
+    print("Progress .... %f%% complete"%(100.0*float(i)/totalResults))
+
+    #print("\n\n\n\n\n")
     url, text = result
     found = False
     toTranslate = []
     s = ""
     other = ""
+
+    """
+
+    Itterate over every character in the text.
+    Try to convert every character to ascii. If a character doesn't
+    convert, catch the exception, and we now know that is something we
+    want to send to the translator. If we build a string of these characters
+    until we hit one that does convert, and isn't punctuation etc. At that
+    poin we are at the end of the arabic text, and can push it to a list.
+
+    Once we have all the arabic (or at least non-ascii) collected, we submit it
+    as a string to the translation server, and then put it back in the database
+    int the englishText column.
+
+    """
+
+    print("extracting text ....%d characters"%len(text))
     for j,char in enumerate(text):
         try:
             other += char
@@ -48,29 +70,41 @@ for i,result in enumerate(results[:]):
             else:
                 if found:
                     found = False
-                    # print("COMPLETE: ",s)
-                    toTranslate.append(s)
+                    if all([ e in specialChars for e in s ]):
+                        toTranslate.insert(s,0)
+                    s=""
         except UnicodeEncodeError:
             if not found:
                 found = True
                 s = ""
 
             s += char
-    # print("COMPLETE: ",s)
-    # print("\n\nOTHER",other)
 
+    if len(s):
+        toTranslate.insert(s,0)
 
+    print("Text extraction complete")
+
+    if not len(toTranslate):
+        continue
+
+    print("Translating: ",url)
+    print("Found %d elements to translate"%len(toTranslate))
+    print("\n".join(toTranslate))
+    
     req = requests.post("http://10.1.90.126:32773/translate", data={"text":batchTranslateDelim.join(toTranslate)})
 
 
     ret = json.loads( req.content )
+    print("Translation complete")
 
     # Put data back in the databse
-    cur.execute("UPDATE urlData set englishText='%s' WHERE url='%s' and domain='%s'"%
+    cur.execute("UPDATE urlData set englishText=? WHERE url=?",
         (
-            ret['translated_text'], result[0], result[3]
+            ret['translated_text'], url
         )
     )
     con.commit()
+    print("URL complete\n\n")
 
 con.close()
